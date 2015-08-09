985217e93d1b5799208a36e98bf2d72a0b57211b
Ilmanfordinner <ilmansonic@abv.bg>
2015-08-09 23:25:30 +0300

    Added Player GameObject
    Added a GameObject, controlled by the Player with Input.GetAxis().

---
+++ Assets/Scripts/BasePlayerController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasePlayerController : MonoBehaviour 
+{
+	public float acceleration;
+	public float maxMovementSpeed;
+
+	Rigidbody2D rigidBody;
+
+
+	void Start () 
+	{
+		rigidBody = GetComponent<Rigidbody2D> ();
+	}
+
+	void FixedUpdate()
+	{
+		Vector2 changeVelocity = new Vector2 (Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical"));	//creates a Vector2 with the change to velocity (if there is any)
+		changeVelocity = changeVelocity.normalized * acceleration;		//applies acceleration to changeVelocity
+
+		rigidBody.velocity = rigidBody.velocity + changeVelocity;
+		if (rigidBody.velocity.magnitude > maxMovementSpeed) 			//caps the movementSpeed
+		{
+			rigidBody.velocity = rigidBody.velocity.normalized*maxMovementSpeed;
+		}
+	}
+
+
+	void Update () 
+	{
+	
+	}
+}
