32a554a57ac4ea0c7b9fd989d8a1dd2ed95d796d
Wang33 <petroverik@abv.bg>
2015-08-10 00:19:16 +0300

    Created bullet shooting system
    Wang

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletController : MonoBehaviour {
+
+
+	public float bulletSpeed;
+	public Camera mainCamera; // Init
+	public GameObject bullet;
+
+	void Update ()
+	{
+		Vector2 mousePos = mainCamera.ScreenToWorldPoint(Input.mousePosition); // Get mouse position according to the camera
+
+		float bulletAngle = Mathf.Atan2(mousePos.y - transform.position.y, mousePos.x - transform.position.x) * 180 / Mathf.PI; // Get angle from mouse and player positions
+
+		if(Input.GetButtonDown("Fire1")) // Fires bullets
+		{
+			Rigidbody2D rigidBody; // Create rigidbody for bullet
+			rigidBody = (Instantiate(bullet, transform.position, Quaternion.Euler(0, 0, bulletAngle)) as GameObject).GetComponent<Rigidbody2D> (); // Create bullet 
+
+			Vector2 bulletDirection = mousePos - new Vector2(transform.position.x, transform.position.y); // Create vector with direction from mouse and player
+
+			bulletDirection = bulletDirection.normalized * bulletSpeed; // Normalizes vector and multiplay it by bullet speed
+
+			rigidBody.velocity = bulletDirection; // Bullet moves according to the vector
+		}
+
+	}
+}
